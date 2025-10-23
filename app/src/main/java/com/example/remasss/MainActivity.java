1a5ca14602127ecb84ec3d988804f7291c9c45fc
class01 <remasali476@gmail.com>
2025-10-23 08:23:55 +0300

    dhdjdeh

--- app/src/main/java/com/example/remasss/MainActivity.java
+++ app/src/main/java/com/example/remasss/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.remasss;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,7 @@ private EditText et_Input;
     private MenuItem itemSettings;
     private MenuItem itemSignOut;
     private MenuItem itemHistory;
+    private static final String TAG = "LifecycleLab";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -48,15 +51,73 @@ private EditText et_Input;
         getMenuInflater().inflate(R.menu.remasmenu, menu);
         return true;
     }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.itemHistory)
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId() == R.id.itemSettings)
         {
+            Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this,Settings.class);
+            startActivity(i);
         }
         if (item.getItemId() == R.id.itemSignOut)
         {
+            Toast.makeText(this,"Sign Out" , Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this,SignOut.class);
+            startActivity(i);
         }
+        if(item.getItemId()==R.id.itemHistory)
+        {
+            Toast.makeText(this, "History", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(MainActivity.this,History.class);
+            startActivity(i);
+        }
+
         return true;
     }
 }
+
+
+
--- app/src/main/java/com/example/remasss/SecondActivity.java
+++ app/src/main/java/com/example/remasss/SecondActivity.java
@@ -5,6 +5,7 @@ import static androidx.core.content.ContextCompat.startActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,6 +29,7 @@ public class SecondActivity extends AppCompatActivity {
     private MenuItem itemSettings;
     private MenuItem itemSignOut;
     private MenuItem itemHistory;
+    private Button btnModolo;
 
 
     @SuppressLint("MissingInflatedId")
@@ -41,6 +43,20 @@ public class SecondActivity extends AppCompatActivity {
         Multiply = findViewById(R.id.Multiply);
         Mul = findViewById(R.id.Mul);
         Result = findViewById(R.id.Result);
+        btnModolo=findViewById(R.id.btnModolo);
+        btnModolo .setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                String s1 = num1.getText().toString();
+                String s2 = num2.getText().toString();
+                int n1 = Integer.parseInt(s1);
+                int n2 = Integer.parseInt(s2);
+                int res = n1 % n2;
+                Result.setText(String.valueOf(res));
+            }
+        });
     }
 
 
@@ -71,16 +87,27 @@ public class SecondActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        if (item.getItemId() == R.id.itemHistory)
+        if (item.getItemId() == R.id.itemSettings)
         {
-            Toast.makeText(this,"I am SettingS", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();
+             Intent i = new Intent(SecondActivity.this,Settings.class);
+             startActivity(i);
         }
         if (item.getItemId() == R.id.itemSignOut)
         {
-            Toast.makeText(this,"I am logOut" , Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Sign Out" , Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(SecondActivity.this,SignOut.class);
+            startActivity(i);
+        }
+        if(item.getItemId()==R.id.itemHistory)
+        {
+            Toast.makeText(this, "History", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(SecondActivity.this,History.class);
+            startActivity(i);
         }
 
         return true;
